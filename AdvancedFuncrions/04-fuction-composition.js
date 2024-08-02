747ce6d9856759d5d46cee947a3556d87a6525b7
NGNikolay <georgiev.nikolaj@gmail.com>
2024-08-02 11:07:44 +0300

    added arrow function solution

--- AdvancedFuncrions/04-fuction-composition.js
+++ AdvancedFuncrions/04-fuction-composition.js
@@ -1,19 +1,20 @@
-function double(num) {
-  return num * 2;
-}
-
-function square(num) {
-  return num ** 2;
-}
-
-function compose(doubleFn, squareFn) {
-  return (n) => squareFn(doubleFn(n));
-}
-
+const double = (num) => num * 2;
+const square = (num) => num ** 2;
+const compose = (double, square) => (n) => square(double(n)); 
 const doubleThenSquare = compose(double, square);
+console.log(doubleThenSquare(3));
 
+// function double(num) {
+//   return num * 2;
+// }
+  
+// function square(num) {
+//   return num ** 2;
+// }
+
+    // function compose(doubleFn, squareFn) {
+      //   return (n) => squareFn(doubleFn(n));
+    // }
 // function doubleThenSquare(num) {
 //   return square(double(num));
 // }
-
-console.log(doubleThenSquare(3));
