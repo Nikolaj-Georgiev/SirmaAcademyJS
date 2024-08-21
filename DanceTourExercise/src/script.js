8c296ce70d4d9e931e423b4a95b3f4aff69bd4a1
Nikolay Georgiev <georgiev.nikolaj@gmail.com>
2024-08-21 10:01:54 +0300

    made sticky nave to show up after the hero section

--- DanceTourExercise/src/script.js
+++ DanceTourExercise/src/script.js
@@ -1,14 +1,57 @@
-window.addEventListener('scroll', function () {
-  const navbar = document.getElementById('navbar');
-  const sticky = navbar.offsetTop;
+//////////////////////////////////////////////////////////////
+// Smooth scrolling animation
+const links = document.querySelectorAll('a[href^="#"]');
+const offset = 5 * parseFloat(getComputedStyle(document.documentElement).fontSize); // 5rem
 
-  if (window.scrollY > sticky) {
-    navbar.classList.add('sticky');
-  } else {
-    navbar.classList.remove('sticky');
-  }
+links.forEach(link => {
+  link.addEventListener('click', function (event) {
+    event.preventDefault();
+    const targetId = this.getAttribute('href').substring(1);
+    if (targetId === '') {
+      // scroll to top if href="#"
+      window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+      });
+    } else {
+      const targetElement = document.getElementById(targetId);
+
+      if (targetElement) {
+        window.scrollTo({
+          top: targetElement.offsetTop - offset,
+          behavior: 'smooth'
+        });
+      }
+    }
+  });
 });
 
+
+////////////////////////////////////////////////////////////
+// Sticky navigation
+const sectionHeroEl = document.querySelector('.hero');
+const navbar = document.getElementById('navbar');
+
+const observer = new IntersectionObserver(function (entries) {
+  const ent = entries[0];
+  if (!ent.isIntersecting) {
+    // console.log(ent);
+    navbar.classList.add('sticky');
+  }
+
+  if (ent.isIntersecting) {
+    navbar.classList.remove('sticky');
+  }
+}, {
+  // In the viewport
+  root: null,
+  threshold: 0,
+  rootMargin: '-80px',
+});
+observer.observe(sectionHeroEl);
+
+//////////////////////////
+// Burger menu
 const checkbox = document.querySelector('.menu-toggle');
 const navLinks = document.querySelector('.header-ul');
 navLinks.addEventListener('click', (e) => {
