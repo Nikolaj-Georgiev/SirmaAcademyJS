1bb8b9612d7eed1c332f75943affcdf5f5536cad
Nikolay Georgiev <georgiev.nikolaj@gmail.com>
2024-08-25 00:15:37 +0300

    added tailwind ui lib

--- React/src/main.jsx
+++ React/src/main.jsx
@@ -1,10 +1,10 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import './index.css';
+import { createRoot } from 'react-dom/client';
+import App from './App.jsx';
+import { ThemeProvider } from '@material-tailwind/react';
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+  <ThemeProvider>
     <App />
-  </StrictMode>,
-)
+  </ThemeProvider>
+);
---
+++ React/tailwind.config.js
@@ -0,0 +1,10 @@
+import withMT from "@material-tailwind/react/utils/withMT";
+
+/** @type {import('tailwindcss').Config} */
+export default withMT({
+  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+});
