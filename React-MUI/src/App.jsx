45728841d773e53d20aff4009a23c836e27ebc76
Nikolay Georgiev <georgiev.nikolaj@gmail.com>
2024-08-25 00:33:01 +0300

    install mui components library

--- React-MUI/src/App.jsx
+++ React-MUI/src/App.jsx
@@ -1,12 +1,12 @@
-import { Button } from "@material-tailwind/react";
+import Button from '@mui/material/Button';
 
 function App() {
-    return (
-        <>
-            <h1>Yooo yo ma maaaan!!!</h1>
-            <Button>Button</Button>
-        </>
-    );
+  return (
+    <>
+      <h1>Yooo yo ma maaaan!!!</h1>
+      <Button variant='contained'>Hello world</Button>
+    </>
+  );
 }
 
 export default App;
--- React-MUI/src/main.jsx
+++ React-MUI/src/main.jsx
@@ -1,5 +1,9 @@
-import './index.css';
+import './main.css';
 import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
 
 createRoot(document.getElementById('root')).render(<App />);
