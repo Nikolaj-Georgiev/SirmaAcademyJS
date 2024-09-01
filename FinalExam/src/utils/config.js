ebef2da99ea46089520a73ad3612d540f9540bb4
Nikolay Georgiev <georgiev.nikolaj@gmail.com>
2024-09-01 19:12:52 +0300

    added array with country names and their country code

--- FinalExam/src/utils/config.js
+++ FinalExam/src/utils/config.js
@@ -1,1 +1,28 @@
 export const CUTOFF_DATE = '6/26/2024';
+
+const TEAMS = [
+  { name: 'Germany', code: 'de' },
+  { name: 'Scotland', code: 'gb-sct' },
+  { name: 'Hungary', code: 'hu' },
+  { name: 'Switzerland', code: 'ch' },
+  { name: 'Spain', code: 'es' },
+  { name: 'Croatia', code: 'hr' },
+  { name: 'Italy', code: 'it' },
+  { name: 'Albania', code: 'al' },
+  { name: 'Slovenia', code: 'si' },
+  { name: 'Denmark', code: 'dk' },
+  { name: 'Serbia', code: 'rs' },
+  { name: 'England', code: 'gb-eng' },
+  { name: 'Poland', code: 'pl' },
+  { name: 'Netherlands', code: 'nl' },
+  { name: 'Austria', code: 'at' },
+  { name: 'France', code: 'fr' },
+  { name: 'Belgium', code: 'be' },
+  { name: 'Slovakia', code: 'sk' },
+  { name: 'Romania', code: 'ro' },
+  { name: 'Ukraine', code: 'ua' },
+  { name: 'Turkey', code: 'tr' },
+  { name: 'Georgia', code: 'ge' },
+  { name: 'Portugal', code: 'pt' },
+  { name: 'Czech Republic', code: 'cz' }
+];
