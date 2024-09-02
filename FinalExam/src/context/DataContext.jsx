d704e0e822296ddae7bc9a4d3f764c179b59f268
Nikolay Georgiev <georgiev.nikolaj@gmail.com>
2024-09-02 12:49:47 +0300

    added guards in the context provider for loading and error

--- FinalExam/src/context/DataContext.jsx
+++ FinalExam/src/context/DataContext.jsx
@@ -2,6 +2,8 @@ import { createContext, useContext } from 'react';
 import useCSVData from '../hooks/useCSVData';
 import useGetTeamsFlags from '../hooks/useGetTeamsFlags';
 import { TEAMS } from '../utils/config';
+import Error from '../components/Error';
+import Loader from '../components/Loader';
 
 const DataContext = createContext({
   playersData: [],
@@ -20,6 +22,23 @@ export const DataProvider = ({ children }) => {
   const flagUrls = useGetTeamsFlags(TEAMS);
   console.log(flagUrls);
 
+  if (
+    playersData.loading ||
+    teamsData.loading ||
+    recordsData.loading ||
+    matchesData.loading
+  ) {
+    return <Loader />;
+  }
+
+  if (
+    playersData.error ||
+    teamsData.error ||
+    recordsData.error ||
+    matchesData.error
+  ) {
+    return <Error />;
+  }
   return (
     <DataContext.Provider
       value={{ teamsData, matchesData, playersData, recordsData, flagUrls }}
