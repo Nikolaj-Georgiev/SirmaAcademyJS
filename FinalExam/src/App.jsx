6534f195a6ce94972fa744a7a2684d4e53c24a3c
Nikolay Georgiev <georgiev.nikolaj@gmail.com>
2024-09-04 12:17:02 +0300

    added bracket folder and removed some dummy data

--- FinalExam/src/App.jsx
+++ FinalExam/src/App.jsx
@@ -6,17 +6,9 @@ import Header from './components/Header';
 import Players from './components/Players';
 import HomePage from './components/HomePage';
 import Groups from './components/Groups';
-import Bracket from './components/Bracket';
+import Bracket from './components/bracket/Bracket';
 
 function App() {
-  // TODO: - deside should you pass data to the bracket from here, or get it in the component.!!(Second is better I think)
-  const matches = [
-    { team1: 'Team A', team5: 'Team B' },
-    { team2: 'Team C', team6: 'Team D' },
-    { team3: 'Team A', team7: 'Team B' },
-    { team4: 'Team A', team8: 'Team B' },
-  ];
-
   return (
     <>
       <Header />
@@ -39,7 +31,7 @@ function App() {
         />
         <Route
           path='/bracket'
-          element={<Bracket matches={matches} />}
+          element={<Bracket />}
         />
         <Route
           path='/players'
