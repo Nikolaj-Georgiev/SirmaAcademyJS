f91e93c08f609a55971e9573e1177bbdee0c858b
Nikolay Georgiev <georgiev.nikolaj@gmail.com>
2024-09-04 15:39:53 +0300

    added flex to phase and next steps for the styling

--- FinalExam/src/components/bracket/BracketPhase.jsx
+++ FinalExam/src/components/bracket/BracketPhase.jsx
@@ -6,10 +6,10 @@ export default function BracketPhase({ phase, phaseName, cssId }) {
   return (
     <div className={`bracket__phase bracket__phase--${cssId}`}>
       <h3>{phaseName}</h3>
-      {Object.entries(phase).map(([date, matches]) => (
+      {Object.entries(phase).map(([date, matches], index) => (
         <div
           key={date}
-          className='bracket__phase-date'
+          className={`bracket__phase-date bracket__phase-date--${index + 1}`}
         >
           <h4>{date}</h4>
           <BracketLines
